cd2cd098227db0b6b8fe21117349b70c13d1ba98
Ehsan Elhampour <ehsan.elhampour@gmail.com>
2017-02-04 16:06:37 +0330

    init

---
+++ SampleQueryString/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SampleQueryString.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string q)
+        {
+            return View();
+        }
+    }
+}
